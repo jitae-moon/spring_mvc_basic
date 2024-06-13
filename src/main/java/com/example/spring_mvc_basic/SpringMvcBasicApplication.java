7bbec3b7c187134e1d28f0c56823715a3b1ba1a4
jitae-moon <jitae.moon@icloud.com>
2024-06-13 14:30:04 +0900

    Servlet을 인식할 수 있도록 프로젝트 클래스에 @ServletComponentScan 추가.

--- src/main/java/com/example/spring_mvc_basic/SpringMvcBasicApplication.java
+++ src/main/java/com/example/spring_mvc_basic/SpringMvcBasicApplication.java
@@ -2,7 +2,9 @@ package com.example.spring_mvc_basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // Servlet 자동 등록
 @SpringBootApplication
 public class SpringMvcBasicApplication {
 
